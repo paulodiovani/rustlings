2ae03a4c35f6da911fcc3d3ab79ccf3e3a033813
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-09-11 16:47:11 -0300

    WIP: conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let chunks = s.split(",");
+        match chunks.collect::<Vec<&str>>()[..] {
+            ["", ..] => Person::default(),
+            [_name, "", ..] => Person::default(),
+            [name, age, ..] => {
+                if let Ok(age) = age.parse::<usize>() {
+                    Person {
+                        name: name.into(),
+                        age,
+                    }
+                } else {
+                    Person::default()
+                }
+            }
+            _ => Person::default(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,18 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let chunks = s.split(",");
+        match chunks.collect::<Vec<&str>>()[..] {
+            ["", _age] => Err(ParsePersonError::NoName),
+            [name, age] => {
+                match age.parse::<usize>() {
+                    Ok(age) => Ok(Person { name: name.into(), age }),
+                    Err(err) => Err(ParsePersonError::ParseInt(err)),
+                }
+            }
+            [""] => Err(ParsePersonError::Empty),
+            _ => Err(ParsePersonError::BadLen),
+        }
     }
 }
 
